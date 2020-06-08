9221d4f00b13c35acb5e8aa742d4855a7a314aeb
kittimorvai <kittimorv@gmail.com>
2020-06-08 12:14:07 +0200

    Initial commit

---
+++ src/IntEqualityPrinter.java
@@ -0,0 +1,18 @@
+public class IntEqualityPrinter {
+    public static void main(String[] args) {
+        printEqual(-5,5,5);
+    }
+
+    public static void printEqual (int firstNum, int secondNum, int thirdNum){
+        if ((firstNum < 0) || (secondNum < 0) || (thirdNum < 0)){
+            System.out.println("Invalid Value");
+        } else if ((firstNum == secondNum) && (firstNum == thirdNum)){
+            System.out.println("All numbers are equal");
+        } else if (firstNum != secondNum && firstNum != thirdNum && secondNum != thirdNum){
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+
+}
